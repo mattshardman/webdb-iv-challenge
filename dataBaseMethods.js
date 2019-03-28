48513124b7297743251d8096de77e73ec4637eaf
mattshardman <mattshardman@gmail.com>
2019-03-28 21:08:29 +0000

    add error handling

--- dataBaseMethods.js
+++ dataBaseMethods.js
@@ -1,54 +1,66 @@
-const knex = require('knex');
-const knexConfig = require('./knexfile');
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 const db = knex(knexConfig.development);
 
 const getDishes = async () => {
-    const dishes = await db('dishes');
-    return dishes
-}
-
-const getDish = async (id) => {
-    const dishes = await db({ d: 'dishes' })
-        .where('d.id', id)
-        .select('d.id', 'd.name', { recipe_name: 'recipes.name '})
-        .innerJoin('dish-recipe', 'd.id', 'dish-recipe.dish_id')
-        .innerJoin('recipes', 'recipes.id', 'dish-recipe.recipe_id')
+  try {
+    const dishes = await db("dishes");
     return dishes;
-}
+  } catch (e) {
+    return e;
+  }
+};
 
-const addDish = async (dish) => {
-    const newDish = await db
-        .insert({ name: dish })
-        .into('dishes');
+const getDish = async id => {
+  try {
+    const dishes = await db({ d: "dishes" })
+      .where("d.id", id)
+      .select("d.id", "d.name", { recipe_name: "recipes.name " })
+      .innerJoin("dish-recipe", "d.id", "dish-recipe.dish_id")
+      .innerJoin("recipes", "recipes.id", "dish-recipe.recipe_id");
+    return dishes;
+  } catch (e) {
+    return e;
+  }
+};
+
+const addDish = async dish => {
+  try {
+    const newDish = await db.insert({ name: dish }).into("dishes");
     return newDish;
-}
+  } catch (e) {
+    return e;
+  }
+};
 
 const getRecipes = async () => {
-    const recipes = await db({ r: 'recipes' })
-        .select(
-            'r.id', 
-            'r.name', 
-            { dish_name: 'dishes.name' }
-        )
-        .innerJoin('dish-recipe', 'r.id', 'dish-recipe.recipe_id')
-        .innerJoin('dishes', 'dishes.id', 'dish-recipe.dish_id')
+  try {
+    const recipes = await db({ r: "recipes" })
+      .select("r.id", "r.name", { dish_name: "dishes.name" })
+      .innerJoin("dish-recipe", "r.id", "dish-recipe.recipe_id")
+      .innerJoin("dishes", "dishes.id", "dish-recipe.dish_id");
     return recipes;
-}
+  } catch (e) {
+    return e;
+  }
+};
 
-const addRecipe = async (recipe) => {
-    const newRecipe = await db
-        .insert(recipe)
-        .into('recipes');
+const addRecipe = async recipe => {
+  try {
+    const newRecipe = await db.insert(recipe).into("recipes");
 
     return newRecipe;
-}
+  } catch (e) {
+    return e;
+  }
+};
 
-getDish(1)
+getDish(1);
 
 module.exports = {
-    getDishes,
-    getDish,
-    addDish,
-    getRecipes,
-    addRecipe
-}
+  getDishes,
+  getDish,
+  addDish,
+  getRecipes,
+  addRecipe
+};
